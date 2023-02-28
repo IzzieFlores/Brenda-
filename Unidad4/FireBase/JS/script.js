bae646d79350649ea5b3ad10d5328f9ef6ff9555
Brenda <bf6819000@gmail.com>
2023-02-28 11:20:39 -0500

    Nuevos Cambios

--- Unidad4/FireBase/JS/script.js
+++ Unidad4/FireBase/JS/script.js
@@ -12,28 +12,72 @@ $(document).ready(function () {
     // Initialize Firebase
     const app = firebase.initializeApp(firebaseConfig);
     console.log(app);
+
+    //**********Registrar Usuarios*************//
+    //Seleccionando el boton registrar
+    $("#btn-registrar").click(function () {
+        //Capturar el Email y el Password
+        let email = $("#email").val();
+        let password = $("#password").val();
+
+        //  console.log(email, password); //comprobamos si captura datos
+
+
+        //Método de firebase que registra usuarios
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in
+
+                alert('Creaste una cuenta exitosamente');
+                // ...
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // ..
+                console.log(errorCode, errorMessage);
+                alert('Ocurrio un error al crear tu cuenta');
+
+            });
+
+    });
+
+
+
+
+
+    //Ingresar con nuestro correo registrado
+    $("#btn-iniciar").click(function () {
+
+         //Capturar el Email y el Password
+         let email = $("#email").val();
+         let password = $("#password").val();
+ 
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in
+                // alert("Ingresaste");
+                // ...
+                window.location.href = "home.html" 
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                console.log(errorCode, errorMessage);
+
+                alert("No se pudo iniciar sesión");
+            });
+    });
+
+    //Cerrar Sesión
+    $("#salir").click(function(){
+        
+        firebase.auth().signOut().then(() => {
+            // Sign-out successful.
+            window.location.href = "index.html"
+          })
+    });
+    
 });
-
-//**********Registrar Usuarios*************//
-//Seleccionando el boton registrar
-$("#btn-registrar").click(function () {
-    //Capturar el Email y el Password
-    let email = $("#email").val();
-    let password = $("#password").val();
-
-    //  console.log(email, password); //comprobamos si captura datos
-
-
-    //Método de firebase que registra usuarios
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // Signed in
-            var user = userCredential.user;
-            // ...
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // ..
-        });
-})
